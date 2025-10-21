1d8d8a29c953507215608359d02e67b02c6e764e
Temitayo Adebayo <adebayotemitayo86@gmail.com>
2025-10-20 21:20:52 -0400

    deployment error

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,12 +1,5 @@
-// postcss.config.mjs (or .js)
-
-const config = {
-  plugins: [
-    // Use the explicit PostCSS plugin package name
-    "@tailwindcss/postcss",
-    // Include Autoprefixer for browser compatibility
-    "autoprefixer",
-  ],
+export default {
+  plugins: {
+    "@tailwindcss/postcss": {},
+  },
 };
-
-export default config;
